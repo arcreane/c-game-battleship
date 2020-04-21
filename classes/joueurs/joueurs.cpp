5248a5209f915a154653b85411a2a58a7f51f411
goullierj <goullier.julien@gmail.comgit config --global user.email goullier.julien@gmail.com>
2020-04-21 16:13:43 +0200

    class joueur

--- classes/joueurs/joueurs.cpp
+++ classes/joueurs/joueurs.cpp
@@ -1,1 +1,26 @@
-#include "joueurs.h"
+#include "joueur.h"
+
+joueur::joueur(char *nom){
+
+	/*constructeur*/
+
+	strcpy(this->nom,nom);
+	this->nombreBateauxCoule = 0;
+
+}
+
+char *joueur::getNom(void){
+
+	return this->nom;
+
+}
+
+int joueur::getNumBateauxCoule(void){
+
+	return this->nombreBateauxCoule;
+
+}
+
+void joueur::decrementNumBateaux(void){
+    
+    this->nombreBateauxCoule++;
--- classes/joueurs/joueurs.h
+++ classes/joueurs/joueurs.h
@@ -1,5 +1,22 @@
 #pragma once
-class joueurs
+
+class joueurs //declare la class joueur
 {
+    private:
+
+	char nom[255];
+	int nombreBateauxCoule; //nombre de bateaux coulÈs parmis les propres bateaux du joueur
+
+
+public:
+
+    joueur(char *nom); //prototype du constructeur
+	
+	char *getNom(void); //renvoi le nom du joueur sous forme de chaine de caractËres
+	int getNumBateauxCoule(void); //retourne le nombre de bateaux coulé au joueur
+	void decrementNumBateaux(void); //specifie qu'un bateau du joueur est coulé
+
+};
+
 };
 
