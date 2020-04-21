c23048eaecfce115fdf3873f76622f0a97b4d82e
malaurieSaintM <m.stm95280@gmail.com>
2020-04-21 23:27:41 +0200

    update bateau

--- classes/bateau/bateaux.h
+++ classes/bateau/bateaux.h
@@ -1,12 +1,13 @@
 #pragma once
 
+
 /*class qui regroupe es attributs et methodes communes à chaque type de bateau */
-class batiment{
+class bateaux {
 
 protected:
 
-	int numCases,ligne,colonne,orientation,numero,numeroJoueur;
-	bool randomPlacement,coule;
+	int numCases, ligne, colonne, orientation, numero, numeroJoueur;
+	bool randomPlacement, coule;
 
 	_case **listeCases;  //liste des cases de la grilles sur lesquelles le bateau est placé
 
@@ -14,94 +15,86 @@ protected:
 
 public:
 
-//constructeur
-	batiment(int numCase,int numero,int numJ,bool randomCoord){
-		
+	//constructeur
+	bateaux(int numCase, int numero, int numJ, bool randomCoord) {
+
 		this->numCases = numCase;
 		this->numero = numero;
 		this->numeroJoueur = numJ;
 		this->listeCases = new _case*[numCase];
 		this->randomPlacement = randomCoord;
-		this->coule = false;	
-
+		this->coule = false;
 	}
 
-	virtual ~batiment(void){
+	virtual ~bateaux(void) {
 
-		delete [] listeCases;  //la liste de cases est le seul élément isntancié dynamiquement
-                               //on ne doit pas delete le contenu de la liste ici, seulement la liste elle même
+		delete[] listeCases;  //la liste de cases est le seul élément isntancié dynamiquement
+							   //on ne doit pas delete le contenu de la liste ici, seulement la liste elle même
 	}
 
-	bool estCoule(void); 
-	bool getStatus(void); 
+	bool estCoule(void);
+	bool getStatus(void);
 	bool estPlaceParJoueur(void);
-	int getNumero(void); 
-	int getNumeroJoueur(void);
-	int getLigne(void); 
-	int getColonne(void); 
-	int getNumCases(void); 
-	int getOrientation(void); 
-	_case **getCases(void); 
-	_case *getCaseTouchee(void); 
-	void setCoule(bool c); 
-	void setLigne(int ligne); 
-	void setColonne(int colonne); 
+	int getNum(void);
+	int getNumJoueur(void);
+	int getLig(void);
+	int getCol(void);
+	int getNumCases(void);
+	int getOrientation(void);
+	_case **getCases(void);
+	_case *getCaseTouchee(void);
+	void setCoule(bool c);
+	void setLig(int ligne);
+	void setCol(int colonne);
 	void setOrientation(int orientation);
 
-	virtual char *getType(void)=0;
-
-
+	virtual char *getType(void) = 0;
 };
 
-class porteAvion :public batiment{
+class porteAvion :public bateaux {
 
 	/*class porteAvion héritée de batiment*/
 
 public:
 
 
-	porteAvion(int numCase,int numero,int numJ,bool randomCoord) : batiment(numCase,numero,numJ,randomCoord){}
+	porteAvion(int numCase, int numero, int numJ, bool randomCoord) : bateaux(numCase, numero, numJ, randomCoord) {}
 	char *getType(void);
 };
 
 
-class croiseur :public batiment{
+class croiseur :public bateaux {
 
 	/*class croiseur héritée de batiment*/
 
 public:
 
-	croiseur(int numCase,int numero,int numJ,bool randomCoord) : batiment(numCase,numero,numJ,randomCoord){}
+	croiseur(int numCase, int numero, int numJ, bool randomCoord) : bateaux(numCase, numero, numJ, randomCoord) {}
 	char *getType(void);
 
 
 };
 
 
-class sousMarin :public batiment{
+class sousMarin :public bateaux {
 
 	/*class sousMarin héritée de batiment*/
 
 public:
-	sousMarin(int numCase,int numero,int numJ,bool randomCoord) : batiment(numCase,numero,numJ,randomCoord){}
+	sousMarin(int numCase, int numero, int numJ, bool randomCoord) : bateaux(numCase, numero, numJ, randomCoord) {}
 	char *getType(void);
-
-
 
 };
 
 
-class torpilleur :public batiment{
+class torpilleur :public bateaux {
 
 	/*class torpilleur héritée de batiment*/
 
 public:
-	torpilleur(int numCase,int numero,int numJ,bool randomCoord) : batiment(numCase,numero,numJ,randomCoord){}
+	torpilleur(int numCase, int numero, int numJ, bool randomCoord) : bateaux(numCase, numero, numJ, randomCoord) {}
 	char *getType(void);
-
-
 };
 
 
 };
-
